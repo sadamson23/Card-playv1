069a74c1b1e1f4c424a546d450d30605657f561f
Stephen Adamson <80679131+sadamson23@users.noreply.github.com>
2021-03-24 17:21:45 +0100

    Initial commit

---
+++ src/demo.js
@@ -0,0 +1,109 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { blue, red } from "@material-ui/core/colors";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import AlarmIcon from "@material-ui/icons/Alarm";
+import factoryImage from "./chocolate.jpg";
+import { green } from "@material-ui/core/colors";
+import SvgIcon from "@material-ui/core/SvgIcon";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 400
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: "rotate(180deg)"
+  },
+  avatar: {
+    backgroundColor: blue[500]
+  }
+}));
+
+export default function RecipeReviewCard() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            FAS
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="FAS IT: Factory Production Impact !!"
+        subheader="April, 2021"
+      />
+      <CardMedia className={classes.media} image={factoryImage} title="mmm" />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          WARNING: Critical OT Server used in chocolate factory processing.
+          Please contact FAS IT for any patching or restarts. Click Teams button
+          to initiate the channel conversation with this team.
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <AlarmIcon />
+        </IconButton>
+        <IconButton aria-label="share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Support Information:</Typography>
+          <Typography paragraph>Factory Location.</Typography>
+          <Typography paragraph>Factory Contacts.</Typography>
+          <Typography paragraph>
+            Factory IT FAS IT Change Management.
+          </Typography>
+          <Typography>Escalations.</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
